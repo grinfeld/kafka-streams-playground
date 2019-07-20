dcc3ca075509f889e2fe5b4ab5e2a2ac57913539
grinfeld <grin1946>
2019-07-20 17:20:05 +0300

    finally it works :)

--- simple/src/main/java/com/mikerusoft/playground/simple/streams/SimpleEventStream.java
+++ simple/src/main/java/com/mikerusoft/playground/simple/streams/SimpleEventStream.java
@@ -3,6 +3,7 @@ package com.mikerusoft.playground.simple.streams;
 import com.mikerusoft.playground.kafkastreamsinit.JSONSerde;
 import com.mikerusoft.playground.kafkastreamsinit.KafkaStreamUtils;
 import com.mikerusoft.playground.models.events.Event;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KafkaStreams;
@@ -14,10 +15,14 @@ import org.springframework.stereotype.Component;
 
 import java.util.Properties;
 import java.util.UUID;
+import java.util.concurrent.*;
+
+import static org.apache.kafka.streams.KafkaStreams.State.RUNNING;
 
 // sh kafka-console-consumer --bootstrap-server localhost:9092 --topic events-stream --from-beginning --group tempme
 // sh kafka-topics --zookeeper localhost:22181 --topic events-stream --describe
 
+@Slf4j
 @Component("events-thorough")
 public class SimpleEventStream implements Streamable {
 
@@ -33,6 +38,7 @@ public class SimpleEventStream implements Streamable {
         KTable<String, Long> ktable = timeStoreBuilder
                 .stream("events-stream", Consumed.with(Serdes.String(), new JSONSerde<>(Event.class)))
                 .mapValues(Event::getTimestamp)
+                .peek((k,v) -> System.out.println(k + " -- " + v ))
                 .groupByKey()
                 .aggregate(() -> 0L, (key, value, aggregate) -> value < aggregate ? aggregate : value,
                     defineAggregateStore("events-stream-time-store")
@@ -42,9 +48,11 @@ public class SimpleEventStream implements Streamable {
         Topology timeStoreTopology = timeStoreBuilder.build();
         System.out.println("" + timeStoreTopology.describe());
         KafkaStreams timeStream = new KafkaStreams(timeStoreTopology, timeStoreConfig);
-        KafkaStreamUtils.runStream(
-            timeStream
-        );
+        timeStream.start();
+
+        ScheduledExecutorService waitForQueryable = Executors.newScheduledThreadPool(1);
+
+        waitForQueryStoreToBeReady(timeStoreName, timeStream, waitForQueryable);
 
         ReadOnlyKeyValueStore<String, Long> eventTimeStore =
                 timeStream.store(timeStoreName, QueryableStoreTypes.keyValueStore());
@@ -76,5 +84,20 @@ public class SimpleEventStream implements Streamable {
         KafkaStreamUtils.runStream(
             new KafkaStreams(mainStreamTopology, mainStreamConfig)
         );
+    }
+
+    private void waitForQueryStoreToBeReady(String timeStoreName, KafkaStreams timeStream, ScheduledExecutorService waitForQueryable) {
+        try {
+            waitForQueryable.scheduleAtFixedRate(() -> {
+                try {
+                    timeStream.store(timeStoreName, QueryableStoreTypes.keyValueStore());
+                    log.warn("found....");
+                    waitForQueryable.shutdown();
+                } catch (Exception e) {
+                    log.warn("waiting....");
+                }
+            }, 0L, 100L, TimeUnit.MILLISECONDS);// wait min
+            waitForQueryable.awaitTermination(10, TimeUnit.MINUTES);
+        } catch (Exception e) { throw new RuntimeException(e); }
     }
 }
